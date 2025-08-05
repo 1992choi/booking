284e8d670f67d263d64bbcac61843099ae2ea630
choi <younggeunn@naver.com>
2025-08-05 22:03:35 +0900

    초기데이터 insert 쿼리 추가

---
+++ setting/init.sql
@@ -0,0 +1,4 @@
+insert into stock.tb_trade_history (is_sold, sold_price, trade_date, trade_price, created_at, trade_history_id, market_code)
+values  (false, null, '2025-07-30', 163630000.00, '2025-07-30 22:59:00.147980', 1, 'BTC'),
+        (false, null, '2025-07-30', 4242.00, '2025-07-30 23:57:00.275519', 2, 'XRP'),
+        (false, null, '2025-07-31', 5307498.00, '2025-07-31 11:17:52.000000', 3, 'ETH');
