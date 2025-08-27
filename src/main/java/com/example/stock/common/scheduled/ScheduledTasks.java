8ad7779eeb2db5f4e9e41ed4ab45b8cea0c6e502
choi <younggeunn@naver.com>
2025-08-27 21:42:12 +0900

    Health check 용도의 노티 추가

--- src/main/java/com/example/stock/common/scheduled/ScheduledTasks.java
+++ src/main/java/com/example/stock/common/scheduled/ScheduledTasks.java
@@ -1,8 +1,10 @@
 package com.example.stock.common.scheduled;
 
 import com.example.stock.service.market.MarketService;
+import com.example.stock.service.noti.TelegramService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -11,7 +13,12 @@ import org.springframework.stereotype.Component;
 @Component
 public class ScheduledTasks {
 
+    @Value("${batch.notify-enabled}")
+    private boolean isNotifyEnabled;
+
     private final MarketService marketService;
+
+    private final TelegramService telegramService;
 
 //    @Scheduled(cron = "0 0 */3 * * *")
 //    public void sendBalanceToTelegram() {
@@ -31,6 +38,14 @@ public class ScheduledTasks {
     @Scheduled(cron = "0 * * * * *")
     public void executeSell() {
         marketService.executeSell();
+    }
+
+    // @Scheduled(cron = "0 0/30 * * * *")
+    @Scheduled(cron = "0 * * * * *")
+    public void sendNotification() {
+        if (isNotifyEnabled) {
+            telegramService.sendSimpleMessage("Application is healthy.");
+        }
     }
 
 }
